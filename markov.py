5ac052959a8e6ef55ec8e16530392534c2903816
Leonard Xander <xander@kth.se>
2023-12-12 15:24:34 +0100

    added markov.py

---
+++ markov.py
@@ -0,0 +1,121 @@
+"""
+Anmärkningar:
+    
+OBS! I just detta fall blev övergångsmatrisen ganska oanvändbar. Möjligen
+för simpel modell, kanske fel på kod/logik?
+    
+1) Ändringar av själva csv-filen:
+   - Första raden borttagen
+   - "Closingreturn" ändrad till "Closing return"
+
+2) Följande motivering till funktionen power_iteration var onödig men låter den stå kvar
+
+Power iteration is a numerical method to find the dominant eigen vector of a matrix.
+THM 10.5.4 says that there exists an unique q such that P*q = q,
+where P is the transition matrix and q is the steady-state vector.
+This is equivalent with fidning the eigenvector of P with eigenvalue 1 (A*x = lambda*x).
+
+3) Att göra?
+    - Generalisera dimensionen av övergångsmatrisen
+    - Kolla på flera dagar samtidigt
+    - Kolla på flera faktorer (högdimensionell matris? se punkt 4)
+    
+    - Göra investeringsstrategi (generaliserad?) och applicera
+    
+4) Ifall högdimentionell matris skulle anvÃ¤ndas kan det bli svårt att lösa ekvationer mm.
+    Metod som går att tillämpa är MCMC (Monte Carlo Markov Chain).
+    
+5) Valde att använda funktionell programmering, men objektorienterad går också.
+"""
+
+# Import statements
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+# Constants
+regimes = ["Low", "Medium", "High"]
+
+# Functions
+def create_transition_matrix(df, regimes):
+    """
+    Creates a transition matrix
+    :param df: DataFrame containing 'Regime'-column
+    :param regimes: List of regimes
+    :return: DataFrame representing transition matrix 
+    """
+    # Initialize transition matrix
+    transition_matrix = pd.DataFrame(0, columns=regimes, index=regimes)
+
+    # Count transitions between regimes
+    for i in range(2, len(df)): #first value is NaN
+        prev_regime = df.at[i - 1, 'Regime']
+        current_regime = df.at[i, 'Regime']
+        transition_matrix.at[prev_regime, current_regime] += 1
+
+    # Normalize to obtain transition probabilities
+    transition_matrix = transition_matrix.div(transition_matrix.sum(axis=1), axis=0)
+    
+    return transition_matrix
+
+def power_iteration(P, max_iter=1000, tol=1e-6):
+    """
+    Finds the steady-state vector of the transition matrix
+    :param P: Transition matrix
+    :param max_iter: Maximum number of iterations
+    :param tol: Tolerance for iteration difference norm
+    
+    Proof: THEOREM 10.5.3 (Behaviour of P^n*x as n -> infinity)
+    """
+    n = len(P)
+    q = np.ones(n) / n  # Initial guess for the steady-state vector
+
+    for _ in range(max_iter):
+        q_new = np.dot(P, q)
+        q_new /= np.linalg.norm(q_new, 1)  # Normalize to ensure q is a probability distribution
+
+        # Check for convergence
+        if np.linalg.norm(q_new - q, 1) < tol:
+            return q_new
+
+        q = q_new
+
+    raise ValueError("Power iteration did not converge within the specified number of iterations.")
+
+# Main
+if __name__ == "__main__":
+    # Read and format
+    ticker = "SPY"
+    df = yf.download(ticker, start = "2000-01-01", end = "2020-11-21")
+    df.reset_index(inplace=True)
+    # df = pd.read_csv("AAPL.csv", delimiter=",") 
+    # df['Close'] = df['Close'].str.replace(".","").astype(float)
+    
+    # Add column for daily return
+    df['Daily return'] = df['Close'].pct_change()
+    
+    # Divide into 3 quantiles and add column for regimes
+    # df['Regime'] = pd.qcut(df['Daily return'], q=3, labels=regimes)
+    
+    # Define conditions and choices for the 'Regime' column
+    conditions = [
+    (df['Daily return'] < 0),
+    (df['Daily return'] >= 0) & (df['Daily return'] <= 0.005),
+    (df['Daily return'] > 0.005)
+    ]
+
+    choices = ['Low', 'Medium', 'High']
+
+    # Use np.select to assign values to the 'Regime' column based on conditions
+    df['Regime'] = np.select(conditions, choices)
+    
+    # Cretae transition matrix
+    transition_matrix = create_transition_matrix(df, regimes)
+    
+    # Find steady-state vector
+    q = power_iteration(transition_matrix)
+
+    # Check steady-state vector
+    check_if_zero = np.linalg.norm(np.dot(np.eye(len(transition_matrix)) - transition_matrix, q))
